97a031854f253588eb9fb309d0fa4fdf5053ddc9
LuisGustavo1303 <128145954+LuisGustavo1303@users.noreply.github.com>
2024-08-06 08:29:09 -0300

    .

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto');
+
 let tamanhoSenha = 12;
+
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha_botao');
 
 botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho; 
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-     tamanhoSenha = tamanhoSenha-1;
-     numeroSenha.textContent = tamanhoSenha;
+function diminuiTamanho() {
+    tamanhoSenha -= 1;
+    numeroSenha.textContent = tamanhoSenha;
 }
 
-function aumentaTamanho(){
-     tamanhoSenha = tamanhoSenha+1;
-     numeroSenha.textContent = tamanhoSenha;
+function aumentaTamanho() {
+    tamanhoSenha += 1;
+    numeroSenha.textContent = tamanhoSenha;
 }
+
 console.log(botoes);
+
